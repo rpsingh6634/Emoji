57844e99c714568d2486bca5d9e60f850c78bbf9
rpsingh6634 <rpsingh6634@gmail.com>
2023-01-13 21:23:06 +0530

    first commit

---
+++ src/components/Nav.js
@@ -0,0 +1,10 @@
+const Nav =()=>{
+    return(
+       <nav>
+            <h2>EMOJI😈</h2>
+            <a className="git_hub"href="https://github.com/rpsingh6634/Emoji" >Github Source Code</a>
+        </nav>
+    )
+}
+
+export default Nav;
